973640bd6276a3e8e0f040dfe1c8207d041ac307
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-20 14:48:47 +0100

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import style from './App.module.scss'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const vejrUrl =`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
+const [vejrData, setVejrData] = useState(null)
+
+
+useEffect(()=>{
+  fetch(vejrUrl)
+  .then((res)=> res.json())
+  .then((data)=> setVejrData(data))
+}, [])
+
+console.log(vejrData)
+
+  let content;
+  if (vejrData) {
+    content = (
+      <div className={style.vejrStyle}>
+        <h2>{vejrData.current.time}</h2>
+        <p>Timezone: {vejrData.timezone}</p>
+        <p>Temperature: {vejrData.current.temperature_2m}°C</p>
+        <p>Wind Speed: {vejrData.current.wind_speed_10m} km/h</p>
+        <p>Latitude: {vejrData.latitude}</p>
+        <p>Longitude:{vejrData.longitude}</p>
+      </div>
+    );
+  } else {
+    content = <p>Loading weather data...</p>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <main>
+      <section>
+        {content}
+      </section>
+      </main> 
     </>
   )
 }
